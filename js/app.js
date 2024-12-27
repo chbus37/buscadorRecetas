2effdbd60b5d58e1e69fca7c605f5a71fda79c34
Christian Bustos <christianbus37@gmail.com>
2024-12-27 15:09:35 -0300

    proyecto terminado

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ function iniciarApp() {
   obtenerCategorias();
 
   const resultado = document.querySelector("#resultado");
+  const modal = new bootstrap.Modal("#modal", {});
 
   function obtenerCategorias() {
     const url = "https://www.themealdb.com/api/json/v1/1/categories.php";
@@ -39,6 +40,13 @@ function iniciarApp() {
   }
 
   function mostrarRecetas(recetas = []) {
+    // Primero limpiar el HTML
+    limpiarHTML(resultado);
+
+    const heading = document.createElement("H2");
+    heading.classList.add("text-center", "text-black", "my-5");
+    heading.textContent = recetas.length ? "Resultados" : "No hay resultados";
+    resultado.appendChild(heading);
     // Iterar en los resultados
 
     recetas.forEach((receta) => {
@@ -60,6 +68,11 @@ function iniciarApp() {
       const recetaButton = document.createElement("BUTTON");
       recetaButton.classList.add("btn", "btn-danger", "w-100");
       recetaButton.textContent = "Ver receta";
+      // recetaButton.dataset.bsTarget = "#modal";
+      // recetaButton.dataset.bsToggle = "modal";
+      recetaButton.onclick = function () {
+        seleccionarReceta(idMeal);
+      };
 
       // Insertar en el HTML
       recetaCardBody.appendChild(recetaHeading);
@@ -69,6 +82,85 @@ function iniciarApp() {
       recetaContenedor.appendChild(recetaCard);
       resultado.appendChild(recetaContenedor);
     });
+  }
+
+  function seleccionarReceta(id) {
+    const url = `https://themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+    fetch(url)
+      .then((respuesta) => respuesta.json())
+      .then((resultado) => mostrarRecetaModal(resultado.meals[0]));
+  }
+
+  function agregarFavorito(receta) {
+    const favoritos = JSON.parse(localStorage.getItem("favoritos")) ?? [];
+    localStorage.setItem("favoritos", JSON.stringify([...favoritos, receta]));
+  }
+
+  function limpiarHTML(selector) {
+    while (selector.firstChild) {
+      selector.removeChild(selector.firstChild);
+    }
+  }
+
+  function mostrarRecetaModal(receta) {
+    const { idMeal, strInstructions, strMeal, strMealThumb } = receta;
+
+    // Agregar contenido al modal
+    const modalTitle = document.querySelector(".modal .modal-title");
+    const modalBody = document.querySelector(".modal .modal-body");
+    modalTitle.textContent = strMeal;
+    modalBody.innerHTML = `
+      <img class="img-fluid" src="${strMealThumb}" alt="Receta ${strMeal}" />
+      <h3 class="my-3">Instrucciones</h3>
+      <p>${strInstructions}</p>
+      <h3 class="my-3">Ingredientes y Cantidades</h3>
+
+    
+    `;
+
+    const listGroup = document.createElement("UL");
+    listGroup.classList.add("list-group");
+    // Mostrar cantidades e ingredientes
+    for (let i = 1; i <= 20; i++) {
+      if (receta[`strIngredient${i}`]) {
+        const ingrediente = receta[`strIngredient${i}`];
+        const cantidad = receta[`strMeasure${i}`];
+        const ingredienteLi = document.createElement("LI");
+        ingredienteLi.classList.add("list-group-item");
+        ingredienteLi.textContent = `${ingrediente} -- ${cantidad}`;
+        listGroup.appendChild(ingredienteLi);
+      }
+    }
+
+    modalBody.appendChild(listGroup);
+
+    const modalFooter = document.querySelector(".modal-footer");
+    limpiarHTML(modalFooter);
+
+    // Botones de cerrar y favorito
+    const btnFavorito = document.createElement("BUTTON");
+    btnFavorito.classList.add("btn", "btn-danger", "col");
+    btnFavorito.textContent = "Guardar Favorito";
+    modalFooter.appendChild(btnFavorito);
+
+    // Local Storage
+    btnFavorito.onclick = function () {
+      agregarFavorito({
+        id: idMeal,
+        titulo: strMeal,
+        img: strMealThumb,
+      });
+    };
+    const btnCerrarModal = document.createElement("BUTTON");
+    btnCerrarModal.classList.add("btn", "btn-secondary", "col");
+    btnCerrarModal.textContent = "Cerrar";
+    btnCerrarModal.onclick = function () {
+      modal.hide();
+    };
+    modalFooter.appendChild(btnCerrarModal);
+
+    // Muestra el moidal
+    modal.show();
   }
 }
 
